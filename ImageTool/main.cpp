1b894be03bedb0d16c9390aa876b55d7417c2bbb
Adam J. Leuer <aleuer@thoughtworks.com>
2017-11-03 13:48:22 -0500

    Initial Commit

---
+++ ImageTool/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ImageTool
+//
+//  Created by Adam J Leuer on 2017-11-3.
+//  Copyright © 2017 ThoughtWorks, Inc. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
